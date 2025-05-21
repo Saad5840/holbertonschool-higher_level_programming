64842b7d345e64e6323f29e144195a2d444a5e63
Your Name <you@example.com>
2025-05-21 15:19:41 +0000

    3-rectangle.py

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -49,7 +49,7 @@ class Rectangle:
         return 2 * (self.__width + self.__height)
 
     def __str__(self):
-        """Return string representation of the rectangle with '#' characters."""
+        """Return string representation of rectangle with '#' characters."""
         if self.__width == 0 or self.__height == 0:
             return ""
         return "\n".join("#" * self.__width for _ in range(self.__height))
