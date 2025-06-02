ec2568b45d158f2a773d2cab1f25621f2a275b30
Your Name <you@example.com>
2025-06-02 16:36:18 +0000

    0-read_file.py

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module that defines a function to read and print a text file (UTF8)."""
+
+def read_file(filename=""):
+    """Reads a UTF8 text file and prints its contents to stdout."""
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
