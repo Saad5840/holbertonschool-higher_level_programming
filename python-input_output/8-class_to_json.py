08964853b17ee1e388f791fed9bad348b5567e5e
Your Name <you@example.com>
2025-06-03 16:56:31 +0000

    8-class_to_json.py

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 
-"""Module that defines a function to return JSON-serializable dictionary of an object."""
+"""Module that defines a function to return JSON-serializable
+ dictionary of an object."""
 
 
 def class_to_json(obj):
     """Returns the dictionary description with simple data structure
     for JSON serialization of an object."""
     return obj.__dict__
-
