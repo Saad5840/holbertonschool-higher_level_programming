111b673ab904613cacc8dd81f3fa17e89f491b73
Your Name <you@example.com>
2025-06-23 16:21:18 +0000

    10-genre_id_by_show.sql

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,12 @@
--- Select the tv show title and the associated genre_id
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
--- Join tv_show_genres to link shows with their genres
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
--- Order by show title ascending, then genre_id ascending
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+-- List all shows with at least one genre linked
+SELECT
+    tv_shows.title,
+    tv_show_genres.genre_id
+FROM
+    tv_shows
+JOIN
+    tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY
+    tv_shows.title ASC,
+    tv_show_genres.genre_id ASC;
+
