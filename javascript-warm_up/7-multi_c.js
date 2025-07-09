9fbc760ba3d1249f0c79741f352c6e7351824ba6
Your Name <you@example.com>
2025-07-09 17:38:09 +0000

    7-multi_c.js

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 const count = parseInt(process.argv[2], 10);
-if (isNaN(count) || count <= 0) {
+
+if (isNaN(count)) {
   console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < count; i++) {
